d8539f96ce9ba8e1fe988d9b4c533bd2017ad651
noeliacossio <noeliacos_8@hotmail.com>
2023-03-07 16:42:14 -0300

    add

--- routes/users.routes.ts
+++ routes/users.routes.ts
@@ -5,6 +5,7 @@ import {
   me,
   findAllUsers,
   findOneUser,
+  updateUser,
 } from "../controllers/user.controller";
 import { validateAdminAndOp } from "../middlewares/validations";
 const router = express.Router();
@@ -14,5 +15,6 @@ router.post("/login", login);
 router.post("/me", me);
 router.post("/findAll", validateAdminAndOp, findAllUsers);
 router.post("/findOne/:id", validateAdminAndOp, findOneUser);
+router.put("/updateUser", updateUser);
 
 export default router;
